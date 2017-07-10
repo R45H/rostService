57c26faaed8c39e62a6510136c7cfe347c0f987d
olga <debxit@gmail.com>
2017-07-10 17:00:30 +0300

    header

---
+++ app/src/blocks/menu/menu.js
@@ -0,0 +1,23 @@
+// open and close side menu
+$(".menu-toggle").click(function () {
+    $('.side-menu').toggleClass('open');
+    $(".menu-toggle").toggleClass('active');
+    $('body').toggleClass('shadow');
+
+    //close menu on body click
+    $(document).on('click', function (e) {
+        var target = e.target;
+
+        if ($(target).closest(".side-menu").length > 0 ) {
+            return false;
+        } else {
+            $('.menu-toggle').removeClass("active");
+            $('.side-menu').removeClass('open');
+            $('body').removeClass("shadow");
+        }
+
+
+    });
+
+    return false;
+});
--- app/src/script.js
+++ app/src/script.js
@@ -1,3 +1,5 @@
-$(function() {
+$(document).ready(function () {
+
+    @@include('blocks/menu/menu.js')
 
 });
