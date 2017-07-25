10c3411f5e8c818b07e82db802886436b35ca4a4
olga <debxit@gmail.com>
2017-07-25 15:24:57 +0300

    models page ended

---
+++ app/src/blocks/spares-slider/spares-slider.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Vasiliy Tsvetkov on 24.07.2017.
+ */
+$('.spares-slider__wrap').slick({
+    // autoplay: true,
+    // autoplaySpeed: 10000,
+    dots: false,
+    infinite: false,
+    prevArrow: '<div class="spares-slider__prev"></div>',
+    nextArrow: '<div class="spares-slider__next"></div>',
+    appendArrows: '.spares-slider__nav',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                dots: true,
+                arrows: false
+            }
+        },
+        {
+            breakpoint:768,
+            settings: {
+                slidesToShow: 2,
+                dots: false,
+                arrows: true
+            }
+        },
+        {
+            breakpoint: 500,
+            settings: {
+                slidesToShow: 1,
+                dots: true,
+                arrows: false
+            }
+        }
+    ]
+});
